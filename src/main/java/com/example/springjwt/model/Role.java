a8df50a5b58864eaedac474809500b9ee4a936ab
Raffay Syed <raffayhusayn@gmail.com>
2021-12-21 21:15:05 -0500

    Added User and Role model class in the model package

---
+++ src/main/java/com/example/springjwt/model/Role.java
@@ -0,0 +1,56 @@
+package com.example.springjwt.model;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+
+    /*
+    constructors
+     */
+
+    public Role() {
+
+    }
+
+    public Role(long id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    /*
+    setters and getters
+     */
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Role{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/springjwt/model/User.java
@@ -0,0 +1,93 @@
+package com.example.springjwt.model;
+
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.GeneratorType;
+import org.springframework.data.repository.cdi.Eager;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.GenerationType.AUTO;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private long id;
+    private String name;
+    private String username;
+    private String password;
+    @ManyToMany(fetch= FetchType.EAGER )
+    private List<Role> roles  = new ArrayList<>();
+
+    /*
+    Constructors
+     */
+
+    public User() {
+
+    }
+    public User(long id, String name, String username, String password, List<Role> roles){
+        this.id = id;
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
+
+    /*
+    Getters and Setters
+     */
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+}
