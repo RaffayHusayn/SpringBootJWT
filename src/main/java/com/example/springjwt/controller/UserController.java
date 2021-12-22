ef3b1695513f9c1792d0524d5562fc38a920e789
Raffay Syed <raffayhusayn@gmail.com>
2021-12-22 01:47:23 -0500

    Added application.properties for mySQL

--- src/main/java/com/example/springjwt/controller/UserController.java
+++ src/main/java/com/example/springjwt/controller/UserController.java
@@ -1,2 +1,26 @@
-package com.example.springjwt.controller;public class UserController {
+package com.example.springjwt.controller;
+
+import com.example.springjwt.model.User;
+import com.example.springjwt.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserServiceImpl userService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService){
+        this.userService = userService;
+    }
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers(){
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
 }
