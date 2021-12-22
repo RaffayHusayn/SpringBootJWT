2a0ae8f8912208670d602f80611f9c36eee2d29f
Raffay Syed <raffayhusayn@gmail.com>
2021-12-21 19:51:24 -0500

    Initial setup with 6 dependencies
    1. Spring Data JPA
    2. MySQL driver
    3. Spring Web
    4. Spring Security
    5. Spring Boot Dev Tools
    6. Lombok

---
+++ src/main/java/com/example/springjwt/SpringBootJwtBoilerPlateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtBoilerPlateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtBoilerPlateApplication.class, args);
+    }
+
+}
