fc021ab0440b03d8f84e2e0c470db7f1f96746e6
Raffay Syed <raffayhusayn@gmail.com>
2021-12-24 05:51:46 -0500

    Add a way to populate the database without the data.sql using the commandlinerunner interface provided by SpringBoot

--- src/main/java/com/example/springjwt/SpringBootJwtBoilerPlateApplication.java
+++ src/main/java/com/example/springjwt/SpringBootJwtBoilerPlateApplication.java
@@ -1,13 +1,47 @@
 package com.example.springjwt;
 
+import com.example.springjwt.model.Role;
+import com.example.springjwt.model.User;
+import com.example.springjwt.service.UserService;
+import com.example.springjwt.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
-public class SpringBootJwtBoilerPlateApplication {
+public class SpringBootJwtBoilerPlateApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJwtBoilerPlateApplication.class, args);
+
+
     }
 
+
+    @Autowired
+    UserServiceImpl userService;
+    @Override
+    public void run(String... args) throws Exception {
+
+        userService.saveRole(new Role(1, "ROLE_USER"));
+        userService.saveRole(new Role(2, "ROLE_ADMIN"));
+
+        userService.saveUser(new User(1001, "raffay","raffayhusayn","password", new ArrayList<>()));
+        userService.saveUser(new User(1002, "ayesha","ayeshahusayn","password2", new ArrayList<>()));
+        userService.saveUser(new User(1003, "nabia","nabia5","hmhmhm", new ArrayList<>()));
+
+        userService.addRoleToUser("raffayhusayn", "ROLE_USER");
+        userService.addRoleToUser("raffayhusayn", "ROLE_ADMIN");
+        userService.addRoleToUser("ayeshahusayn", "ROLE_USER");
+        userService.addRoleToUser("nabia5", "ROLE_ADMIN");
+
+    }
 }
--- src/main/java/com/example/springjwt/controller/UserController.java
+++ src/main/java/com/example/springjwt/controller/UserController.java
@@ -1,11 +1,15 @@
 package com.example.springjwt.controller;
 
+import com.example.springjwt.model.Role;
 import com.example.springjwt.model.User;
 import com.example.springjwt.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,11 +24,44 @@ public class UserController {
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers(){
+        //.ok means it returns a 200 HTTP response back if it works
         return ResponseEntity.ok().body(userService.getUsers());
     }
 
     @PostMapping("/user/save")
     public ResponseEntity<User> saveUser(@RequestBody User user){
-        return ResponseEntity.ok().body(userService.saveUser(user));
+        //here we want to return HTTP 201 which indicates a resource is created on the server which requires the uri of the created resource
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role){
+        //here we want to return HTTP 201 which indicates a resource is created on the server which requires the uri of the created resource
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/role/save").toUriString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> saveRoleToUser(@RequestBody RoleToUserForm form ){
+        //here we just want to return a HTTP OK reponse with no body
+       userService.addRoleToUser(form.getUser(), form.getRole());
+       return ResponseEntity.ok().build();
+    }
+
+}
+
+//making use of this utility class in the saveRoleToUser method of the controller
+class RoleToUserForm{
+
+    private String role;
+    private String user;
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getUser() {
+        return user;
     }
 }
